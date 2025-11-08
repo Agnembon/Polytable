33a31ab77f3880cf9c57901811a4e379a38b77fc
Zalaya <enol.corzal@gmail.com>
2025-11-08 13:11:45 +0100

    feat: add the examples directory where the users can see useful examples of the library

---
+++ examples/polytable-react-example/src/App.tsx
@@ -0,0 +1,13 @@
+import { Table } from "@polytable/react-package";
+
+export const App = () => {
+  const columns = ["A", "B", "C"];
+  const rows = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+
+  return (
+    <Table
+      shape={{ columns, rows }}
+      onSelectionChange={selection => console.log(selection)}
+    />
+  );
+};
--- packages/polytable-react/src/index.ts
+++
@@ -1,2 +0,0 @@
-export { Table } from "./components/Table";
-export * from "./core/types";
