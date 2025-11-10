ffd88808574933cbaa68d072cd9544de8a8b36e9
Zalaya <enol.corzal@gmail.com>
2025-11-10 22:38:27 +0100

    fix: solve some problems with the imports in the vite.config.ts file

--- packages/polytable-react-package/vite.config.ts
+++ packages/polytable-react-package/vite.config.ts
@@ -1,7 +1,6 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import dts from 'vite-plugin-dts';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
